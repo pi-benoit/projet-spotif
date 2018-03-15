d4e8dbcf85f3d21eed589bd7cf6819c95f59bf3f
BENOIT <pi-benoit@users.noreply.github.com>
2018-03-15 14:24:41 +0100

    Update

--- src/fr/borisbenoit/Main.java
+++ src/fr/borisbenoit/Main.java
@@ -11,21 +11,43 @@ public class Main {
 		Musique mus4 = new Musique("Summer", alb1);
 		Musique mus5 = new Musique("Automn", alb1);
 		Musique mus6 = new Musique("Ending", alb1);
+		
+		System.out.println(alb1);
 
 		Playlist pl = new Playlist("Test");
 		pl.addMusique(mus1);
 		pl.addMusique(mus2);
 		pl.addMusique(mus3);
-		pl.addMusique(mus4);
-		pl.addMusique(mus5);
-		pl.addMusique(mus6);
 
-		System.out.println(pl.getMusiquePlaying());
+
+		pl.afficherToutesMusique();
+		pl.afficherMusiquePlaying();
 		pl.nextMusique();
-		System.out.println(pl.getMusiquePlaying());
-		
-		Menu men=new Menu();
-		men.afficherListMusique();
+		Musique mus7 = new Musique("Eating", alb1);
+		pl.addMusique(mus7);
+		pl.nextMusique();
+		pl.afficherToutesMusique();
+		pl.afficherMusiquePlaying();
+
+		Album alb2 = new Album("Album Number deux");
+		Musique mus40 = new Musique("Summer", alb2);
+		Musique mus50 = new Musique("Automn", alb2);
+		Musique mus60 = new Musique("Ending", alb2);
+
+		System.out.println(alb2);
+
+		pl.addMusique(mus40);
+		pl.addMusique(mus50);
+		pl.addMusique(mus60);
+
+		pl.afficherToutesMusique();
+		pl.nextMusique();
+		pl.nextMusique();
+		pl.nextMusique();
+		pl.afficherToutesMusique();
+
+		new Menu();
+
 	}
 
 }
