e4472e151995cfff609bac23251dad2a5484e799
BENOIT <pi-benoit@users.noreply.github.com>
2018-03-15 09:40:49 +0100

    Add toString + hasCode + equals methods, + Implement addMusique method

--- src/fr/borisbenoit/Album.java
+++ src/fr/borisbenoit/Album.java
@@ -8,7 +8,37 @@ public class Album {
 	private List<Musique> musique;
 
 	public void addMusique(Musique mus) {
-		
+		this.addMusique(mus);
+	}
+
+	@Override
+	public String toString() {
+		return "Album [nom=" + nom + ", musique=" + musique + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Album other = (Album) obj;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		return true;
 	}
 
 	public String getNom() {
